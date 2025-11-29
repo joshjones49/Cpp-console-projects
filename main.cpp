540d507bfe09fbc2da36e2a09015b4e5f86a6945
joshjones49 <joshjones200285@gmail.com>
2025-11-29 09:16:36 -0500

    added logic for user login

--- main.cpp
+++ main.cpp
@@ -8,89 +8,173 @@ void checkAccess(Person p);
 vector<Person> seedPersonData();
 vector<Book> seedBookData();
 Person login(vector<Person> people, int id);
-void displayWelcomeMsg(const Person& admin, bool isDisplayed);
+void displayWelcomeMsg(const Person& p);
 Book createBook(string title, string author, int count);
+Person createPerson(string name, int age, string access, int id);
 
 int main() {
 	// seed data =============================>
 	vector<Book> books = seedBookData();
 	vector<Person> people = seedPersonData();
-	string commands[6] = {
+
+	string adminCmds[9] = {
 		"-out | log out", "-addb | add a book",
-		"-addu | add a user", "-co | check out a book",
-		"-cmds | list commands", "-lb | list books"
+		"-addu | add a user", "-co | check out book",
+		"-cmds | list commands", "-lb | list books",
+		"-lu | list users", "-rb | return book", 
+		"-sd | ends program"
 	};
 
-	Person admin("", 0, "", 0);
+	string userCmds[4] = {
+		"-out | log out", "-cmds | list commands",
+		"-lb | list books", "-sd | ends program"
+	};
 
+	Person user("", 0, "", 0);
+
+	bool userLoggedIn = false;
 	bool adminLoggedIn = false;
-	bool displayWelcome = true;
+	bool programRunning = true;
 
 	int currentUser;
 	cout << "Enter User ID: ";
 	cin >> currentUser;
 	currentUser--; // adjust for 0 index
 	try {
-		admin = login(people, currentUser);
-		checkAccess(admin);
-		adminLoggedIn = true;
+		user = login(people, currentUser);
+		if (user.getAccess() == "Admin") {
+			adminLoggedIn = true;
+			userLoggedIn = false;
+		}
+		else {
+			userLoggedIn = true;
+			adminLoggedIn = false;
+		}
+		checkAccess(user);
+		displayWelcomeMsg(user);
 	} catch (const exception& e) {
 		cerr << "Error: " << e.what() << endl;
 		return 1;
 	}
 
-	while (adminLoggedIn) {
+	while (programRunning) {
+		if (!userLoggedIn && !adminLoggedIn) {
+			cout << endl;
+			cout << "Enter User ID: ";
+			cin >> currentUser;
+			currentUser--; // adjust for 0 index
+
+			try {
+				user = login(people, currentUser);
+				if (user.getAccess() == "Admin") {
+					adminLoggedIn = true;
+					userLoggedIn = false;
+				}
+				else {
+					userLoggedIn = true;
+					adminLoggedIn = false;
+				}
+				checkAccess(user);
+				displayWelcomeMsg(user);
+			}
+			catch (const exception& e) {
+				cerr << "Error: " << e.what() << endl;
+				return 1;
+			}
+		}
+
 		string command;
-		displayWelcomeMsg(admin, displayWelcome);
 		cout << endl;
 		cout << "Enter Command:" << endl;
+		cout << boolalpha;
+		cout << userLoggedIn << endl;
+		cout << adminLoggedIn << endl;
 		cout << endl;
 		cin >> command;
-		if (command == "-out") {
-			cout << endl;
-			cout << "Logging Out: " << admin.getName() << endl;
-			adminLoggedIn = !adminLoggedIn;
-		}
-		else if (command == "-cmds") {
-			cout << "Available commands:" << endl;
-			cout << "====================================" << endl;
-			for (const auto& cmd : commands) {
-				cout << cmd << endl;
-			}
-		}
-		else if (command == "-addb") {
-			string title;
-			string author;
-			int count;
-			cout << "Enter book details" << endl;
-			cout << "Title: ";
-			cin.ignore();
-			getline(cin, title);
-			cout << "Author: ";
-			getline(cin, author);
-			cout << "Count: ";
-			cin >> count;
-			books.push_back(createBook(title, author, count));
-			cout << "====================================" << endl;
-			for (Book b : books) {
-				b.print();
-			}
-			displayWelcome = !displayWelcome;
 
-		} else if (command == "-lb") {
-			cout << "Books in Inventory:" << endl;
-			cout << "====================================" << endl;
-			for (Book b : books) {
-				b.print();
+		if (adminLoggedIn) {
+			if (command == "-out") {
+				cout << endl;
+				cout << "Logging Out: " << user.getName() << endl;
+				adminLoggedIn = false;
 			}
-			displayWelcome = !displayWelcome;
+			else if (command == "-cmds") {
+				cout << "Available commands:" << endl;
+				cout << "====================================" << endl;
+				for (const string& cmd : adminCmds) {
+					cout << cmd << endl;
+				}
+			}
+			else if (command == "-addb") {
+				string title;
+				string author;
+				int count;
+				cout << "Enter book details" << endl;
+				cout << "Title: ";
+				cin.ignore();
+				getline(cin, title);
+				cout << "Author: ";
+				getline(cin, author);
+				cout << "Count: ";
+				cin >> count;
+				books.push_back(createBook(title, author, count));
+				cout << "====================================" << endl;
+
+			}
+			else if (command == "-lb") {
+				cout << "Books in Inventory:" << endl;
+				cout << "====================================" << endl;
+				for (Book b : books) {
+					b.print();
+				}
+			}
+			else if (command == "-addu") {
+				string name;
+				int age;
+				string access;
+				int id;
+				cout << "Enter user details" << endl;
+				cout << "Name: ";
+				cin.ignore();
+				getline(cin, name);
+				cout << "Age: ";
+				cin >> age;
+				cout << "Access Level (Admin/User): ";
+				cin >> access;
+				cout << "ID: ";
+				cin >> id;
+				people.push_back(createPerson(name, age, access, id));
+			}
+			else {
+				cout << "Invalid command. Use -cmds to list commands." << endl;
+				cout << endl;
+			}
 		}
 		else {
-			cout << "Invalid command. Use -cmds to list commands." << endl;
-			cout << endl;
-			displayWelcome = !displayWelcome;
+			if (command == "-out") {
+				cout << endl;
+				cout << "Logging Out: " << user.getName() << endl;
+				userLoggedIn = false;
+			}
+			else if (command == "-cmds") {
+				cout << "Available commands:" << endl;
+				cout << "====================================" << endl;
+				for (const string& cmd : userCmds) {
+					cout << cmd << endl;
+				}
+			}
+			else if (command == "-lb") {
+				cout << "Books in Inventory:" << endl;
+				cout << "====================================" << endl;
+				for (Book b : books) {
+					b.print();
+				}
+			}
+			else {
+				cout << "Invalid command. Use -cmds to list commands." << endl;
+				cout << endl;
+			}	
 		}
-
 	}
 	
 
@@ -139,14 +223,20 @@ Person login(vector<Person> people, int id) {
 	return people.at(id);
 }
 
-void displayWelcomeMsg(const Person& admin, bool isDisplayed) {
-	if (isDisplayed) {
+void displayWelcomeMsg(const Person& p) {
+	if (p.getAccess() == "Admin") {
 		cout << "==========================" << endl;
-		cout << "Welcome: " << admin.getName() << endl;
+		cout << "Welcome: " << p.getName() << endl;
 		cout << "==========================" << endl;
 		cout << endl;
 		cout << "Use -cmds to list commands" << endl;
-		isDisplayed = !isDisplayed;
+	}
+	else {
+		cout << "==========================" << endl;
+		cout << "Welcome: " << p.getName() << endl;
+		cout << "==========================" << endl;
+		cout << endl;
+		cout << "Use -cmds to list commands" << endl;
 	}
 }
 
@@ -155,4 +245,11 @@ Book createBook(string title, string author, int count) {
 	cout << "Book added to inventory" << endl;
 	cout << endl;
 	return b;
+}
+
+Person createPerson(string name, int age, string access, int id) {
+	Person p(name, age, access, id);
+	cout << "User created: " << name << endl;
+	cout << endl;
+	return p;
 }
